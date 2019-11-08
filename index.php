0072cd10a6bc7f2f87b0ca9679192818a099cd01
yosef langer <yosef@doar.systems>
2019-11-08 23:41:50 +0200

    fixes

--- index.php
+++ index.php
@@ -1,22 +1,19 @@
 <?php
 
 
-use CurrencyConverter\CurrencySourceBankOfEurope;
-use CurrencyConverter\CurrencySourceBankOfIsrael;
-
 require_once __DIR__ . "./vendor/autoload.php";
 
-require_once __DIR__ . "./src/api/CurrencySourceBankOfIsrael.php";
-require_once __DIR__ . "./src/api/CurrencySourceBankOfEurope.php";
+require_once __DIR__ . "./src/CurrencySourceBankOfIsrael.php";
+require_once __DIR__ . "./src/CurrencySourceBankOfEurope.php";
 
 
-$converter = new CurrencySourceBankOfEurope();
+$converter = new \vasilevich\currencyconverter\CurrencySourceBankOfEurope();
 
 var_dump($converter->getCurrencyList()->convert("EUR", "USD", 1)); // -> 1 euro to israeli shekels through Europe bank
 var_dump($converter->getCurrencyList()->convert("USD", "EUR", 1)); // -> 1 euro to israeli shekels through Europe bank
 var_dump($converter->getCurrencyList()->convert("ILS", "EUR", 1)); // -> 1 euro to israeli shekels through Europe bank
 var_dump($converter->getCurrencyList()->convert("EUR", "ILS", 1)); // -> 1 euro to israeli shekels through Europe bank
-$converter = new CurrencySourceBankOfIsrael();
+$converter = new \vasilevich\currencyconverter\CurrencySourceBankOfIsrael();
 var_dump($converter->getCurrencyList()->convert("EUR", "USD", 1)); // -> 1 euro to israeli shekels through Europe bank
 var_dump($converter->getCurrencyList()->convert("USD", "EUR", 1)); // -> 1 euro to israeli shekels through Europe bank
 var_dump($converter->getCurrencyList()->convert("ILS", "EUR", 1)); // -> 1 euro to israeli shekels through Europe bank
