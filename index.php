3061d95a1902b164f72c1e47ebb1e8b163e208e5
yosef langer <yosef@doar.systems>
2022-12-26 20:14:39 +0200

    updated bank of israel API to the new api. old deprecated api can still be found in the old file

--- index.php
+++ index.php
@@ -11,6 +11,20 @@ require_once __DIR__ . "/src/CurrencySourceDenemarkNationalBank.php";
 require_once __DIR__ . "/src/CurrencySourceFromSerialization.php";
 
 $converter = new CurrencySourceDenemarkNationalBank();
-
+echo "DENMARK: 1 ILS TO USD: ";
+var_dump($converter->getCurrencyList()->convert("ILS", "USD", 1));
+echo "DENMARK: 1 USD TO ILS: ";
 var_dump($converter->getCurrencyList()->convert("USD", "ILS", 1));
+
+$converter = new \vasilevich\currencyconverter\CurrencySourceBankOfIsrael();
+echo "ISRAEL: 1 ILS TO USD: ";
+var_dump($converter->getCurrencyList()->convert("ILS", "USD", 1));
+echo "ISRAEL: 1 USD TO ILS: ";
+var_dump($converter->getCurrencyList()->convert("USD", "ILS", 1));
+
+
+$converter = new \vasilevich\currencyconverter\CurrencySourceBankOfEurope();
+echo "EUROPE: 1 ILS TO USD: ";
+var_dump($converter->getCurrencyList()->convert("ILS", "USD", 1));
+echo "EUROPE: 1 USD TO ILS: ";
 var_dump($converter->getCurrencyList()->convert("USD", "ILS", 1));
--- src/CurrencySourceBankOfIsrael.php
+++ src/CurrencySourceBankOfIsrael.php
@@ -3,21 +3,29 @@
 namespace vasilevich\currencyconverter;
 require_once __DIR__ . "/CurrencySource.php";
 
+
 class CurrencySourceBankOfIsrael extends CurrencySource
 {
     public function __construct()
     {
-        $this->currencyList = new CurrencyList("https://www.boi.org.il/currency.xml", "ILS");
+        $this->currencyList = new CurrencyList("https://edge.boi.gov.il/FusionEdgeServer/sdmx/v2/data/dataflow/BOI.STATISTICS/EXR/1.0/?lastNObservations=1", "ILS");
         $this->loadAndParseCurrency();
     }
 
     protected function loadAndParseCurrency()
     {
         $url = $this->currencyList->getSource();
-        $xml = simplexml_load_file($url);
+        $content = file_get_contents($url);
+        preg_match_all('/<Series.*?<\/Series>/s', $content, $matches);
+        $result = '<root>' . implode('', $matches[0]) . '</root>';
+        $xml = simplexml_load_string($result);
         foreach ($xml as $element) {
-            if (isset($element->NAME)) {
-                $this->currencyList->add($element->CURRENCYCODE->__toString(), (float)$element->RATE->__toString());
+            $UNIT_MEASURE = (string)$element['UNIT_MEASURE'];
+            $dataSource = (string)$element['DATA_SOURCE'];
+            if ($UNIT_MEASURE = 'ILS' && $dataSource === 'BOI_MRKT') {
+                $currencyCode = (string)$element['BASE_CURRENCY'];
+                $rate = (float)((string)$element->Obs['OBS_VALUE']);
+                $this->currencyList->add($currencyCode, $rate);
             }
         }
         $this->updateTimeStamp();
